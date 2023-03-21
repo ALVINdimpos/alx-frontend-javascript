ce479095d82068b6bcb3e2aaa5790991fac179a7
ALVINdimpos <alvindimpos@gmail.com>
2023-03-21 09:40:04 +0200

    Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,4 @@
+//Keep every promise you make and only make promises you can keep
 export default function getResponseFromAPI() {
   const success = true;
   return new Promise((resolve, reject) => {
