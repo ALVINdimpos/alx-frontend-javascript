e954e2ca01beb6b6833f82f94cd71f8092093de4
ALVINdimpos <alvindimpos@gmail.com>
2023-03-21 09:53:11 +0200

    Add tests for ES6 Promises
    This commit adds a comprehensive suite of tests for the ES6 Promise methods included in this repository. These tests cover a range of scenarios and edge cases to ensure the reliability and accuracy of the code. The tests were written using Jest and can be run using the 'npm test' command. This commit also includes minor adjustments to the README file to reflect the addition of the tests.

--- 0x01-ES6_promise/tests/0-main.js
+++ 0x01-ES6_promise/tests/0-main.js
@@ -1,4 +1,3 @@
-import getResponseFromAPI from "./0-promise.js";
-
+import getResponseFromAPI from "./0-promise.js"; 
 const response = getResponseFromAPI();
 console.log(response instanceof Promise);
