d3764a22ea0ce5276c74996ef23ef3e1c14ade4c
ALVINdimpos <alvindimpos@gmail.com>
2023-03-21 09:33:31 +0200

    Simplify object creation by omitting empty object initialization
    This commit streamlines object creation in the project by omitting the need to create empty objects before adding properties. In JavaScript, it is not necessary to initialize an object with empty curly braces before adding properties to it.

--- 0x00-ES6_basic/tests/101-main.js
+++ 0x00-ES6_basic/tests/101-main.js
@@ -3,7 +3,6 @@ import createReportObject from './12-createReportObject.js';
 import createIteratorObject from './100-createIteratorObject.js';
 import iterateThroughObject from './101-iterateThroughObject.js';
 
-
 const employees = {
     ...createEmployeesObject('engineering', ['Bob', 'Jane']),
     ...createEmployeesObject('marketing', ['Sylvie'])
